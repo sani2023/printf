6680c695f55e3b40a9534f37f061d92a9d2eb36f
el-ngoca <eduardongoca4@gmail.com>
2023-03-27 14:16:13 -0700

    THis is the main header file and functions

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+//THE ABOVE LINE DEFINES THE MAIN HEADER FILE NAME
+//BELLOW ARE THE LIBRARIES THAT WE WILL NEED
+#include <stdio.h>
+#include <unistd.h>
+//BELLOW ARE THE FUNCTIONS FOR THIS PROJECT
+int _printf(const char *format, ...);
+int _putchar(char c);
+
+#endif
